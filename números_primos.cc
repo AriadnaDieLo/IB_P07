45a55378d35213b27f206e5e0ea3df7a35cac753
Ariadna Dielingen <72440018+AriadnaDieLo@users.noreply.github.com>
2020-11-16 06:41:03 +0000

    Create números_primos.cc

---
+++ números_primos.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include "numeros_primos.h"
+using namespace std;
+
+bool IsPrime(int numero_test){
+  bool primo;
+  int prueba = 0;
+  for(int i = 1; i <= numero_test; ++i ) {
+  if( (numero_test % i) == 0) {
+    ++prueba;
+    }
+  if (prueba == 2){
+    primo = true;
+  } else }
+    primo = false;
+    }
+return primo;
+}
